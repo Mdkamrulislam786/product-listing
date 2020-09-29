8c8460cb07acc0a97a3b763b712ac9ebbffdea5c
Mdkamrulislam786 <kamrulislam.ki01@gmail.com>
2020-09-30 03:19:52 +0600

    api integretion done

--- src/App.js
+++ src/App.js
@@ -1,52 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import requests from "./requests";
+import axios from "./axios";
 //COMPONENTS
 import Card from "./Card/Card";
 function App() {
+  const [movies, setMovies] = useState([]);
+  let fetchUrl = requests.fetchNetflixOriginals;
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  console.log(movies);
   return (
     <div className="app">
       <div className="home__section">
-        <Card
-          src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
-          title="Online Experiences"
-          description="September 11 | 9pm"
-          price="Free"
-        />
-        <Card
-          src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
-          title="Online Experiences"
-          description="September 11 | 9pm"
-          price="$12 onwards"
-        />
-        <Card
-          src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
-          title="Online Experiences"
-          description="September 11 | 9pm"
-          price="$12 onwards"
-        />
+        {movies.slice(0, 3).map((movie) => (
+          <Card key={movie.id} {...movie} price="Free" />
+        ))}
       </div>
       <div className="home__section">
-        <Card
-          src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
-          title="Online Experiences"
-          description="September 11 | 9pm"
-          price="Free"
-        />
-        <Card
-          src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
-          title="Online Experiences"
-          description="September 11 | 9pm"
-          price="$12 onwards"
-        />
-        <Card
-          src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
-          title="Online Experiences"
-          description="September 11 | 9pm"
-          price="$12 onwards"
-        />
+        {movies.slice(3, 6).map((movie) => (
+          <Card key={movie.id} {...movie} price="Free" />
+        ))}
       </div>
     </div>
   );
 }
 
 export default App;
+// src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
+// title="Netflix Originals"
+// fetchUrl={requests.fetchNetflixOriginals}
+// price="Free"
+// description="September 11 | 9pm"
--- src/Card/Card.js
+++ src/Card/Card.js
@@ -3,18 +3,34 @@ import "./Card.css";
 import { IconButton } from "@material-ui/core";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 
-const Card = ({ src, title, description, price }) => {
+const baseUrl = "https://image.tmdb.org/t/p/original/";
+
+const Card = ({
+  original_name,
+  first_air_date,
+  backdrop_path,
+  vote_average,
+}) => {
   return (
     <div className="card">
-      <img src={src} alt="img" />
+      <div className="container">
+        <img
+          src={`${baseUrl}${backdrop_path}`}
+          className="image"
+          alt={original_name}
+        />
+        <div className="middle">
+          <div className="text">PLAY</div>
+        </div>
+      </div>
       <div className="card__info">
-        <h2>{title}</h2>
-        <h4> {description} </h4>
+        <h2>{original_name}</h2>
+        <h4> {first_air_date} </h4>
         <h4>
           <a href="#none">Watch on zoom</a>{" "}
         </h4>
         <div className="card__bottom">
-          <h4>{price} </h4>
+          <h4>imdb: {vote_average} </h4>
           <IconButton>
             <FavoriteBorderIcon />
           </IconButton>
